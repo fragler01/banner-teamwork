6dd5edb278095c7bf3da59e7711f526e64b1dadf
fragler01 <60200282+fragler01@users.noreply.github.com>
2022-09-03 16:02:22 +0200

    jav

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let slider = document.getElementById("slider").value;
-slider.value = 50;
 
 function getSliderValue(){
-    let slider = document.getElementById("slider").value;
+    let slider = document.getElementById("slider");
     let kepek = document.getElementById("kepek");
 
-    slider.style.animationDuration = slider.value + "s";
+    kepek.style.animationDuration = slider.value + "s";
     document.getElementById("valasztott").innerHTML = slider.value;
 }
